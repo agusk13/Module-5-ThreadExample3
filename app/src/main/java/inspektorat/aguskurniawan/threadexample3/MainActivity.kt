95ef09ca29840ff7064a70befdb6d3062dc5466e
agusk13 <agusk1308@gmail.com>
2025-11-01 09:39:48 +0700

    ThreadExample3

---
+++ app/src/main/java/inspektorat/aguskurniawan/threadexample3/MainActivity.kt
@@ -0,0 +1,27 @@
+package inspektorat.aguskurniawan.threadexample3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import inspektorat.aguskurniawan.threadexample3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartThread.setOnClickListener {
+            startThread()
+        }
+    }
+
+    private fun startThread(){
+        val runnable = ExampleRunnable(10);
+        Thread(runnable).start()
+    }
+}
